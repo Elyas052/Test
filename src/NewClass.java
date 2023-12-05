0b7b90bd3e2ebf42c6aea550eefdb99114fedf4e
nihad <nihadbaba.2800@gmail.com>
2023-12-04 22:01:44 -0500

    my commit

---
+++ src/NewClass.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class NewClass {
+    public static void main(String[] args) {
+       int[] intArr = {1,2,3,4};
+        sumOfNum(intArr);
+    }
+    public static int sumOfNum(int[] arr){
+        int sum = 0;
+        for (int each: arr){
+            sum += each;
+        }
+        return sum;
+    }
+}
