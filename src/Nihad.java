5c44ff125c73107b37a7f3f8843e8aff286344bc
nihad <nihadbaba.2800@gmail.com>
2023-12-04 21:33:17 -0500

    my commit

---
+++ src/Nihad.java
@@ -0,0 +1,10 @@
+public class Nihad {
+    public static void main(String[] args) {
+        System.out.println(toUpperCase("hello world"));
+    }
+
+
+    public static String toUpperCase(String str){
+        return  str.toUpperCase();
+    }
+}
