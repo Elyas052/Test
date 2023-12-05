d90cb6ee8e2c862939c075f8176ed17eb109f92a
Elyas052 <elyasshiraliyev@gmail.com>
2023-12-04 21:20:31 -0500

    test

---
+++ src/Elyas.java
@@ -0,0 +1,11 @@
+public class Elyas {
+
+    public static int sumOfArray(int[] arr) {
+        int sum = 0;
+
+        for (int eachNum : arr) {
+            sum += eachNum;
+        }
+        return sum;
+    }
+}
